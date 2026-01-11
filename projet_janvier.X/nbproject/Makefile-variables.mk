17e15e79090b0935769375e33bebedf768cc40b0
louismingot <louismingot0704@gmail.com>
2026-01-11 19:16:25 +0100

    initial commit

---
+++ projet_janvier.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=projet_janvier.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/projet_janvier.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projetjanvier.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projetjanvier.x.tar
